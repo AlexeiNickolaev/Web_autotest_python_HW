f58f5743d9ccddf0ed8e126b6d3f5c449b678953
Alexei <datlife2015@yandex.ru>
2023-10-14 15:30:49 +0300

    homework_2

--- homework_2/module.py
+++ homework_2/module.py
@@ -6,30 +6,30 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
 
-with open("testdata.yaml") as f:
+with open('testdata.yaml') as f:
     testdata = yaml.safe_load(f)
-    browser = testdata["browser"]
+    browser = testdata['browser']
 
 
 class Site:
     def __init__(self, address):
-        if browser == "firefox":
+        if browser == 'firefox':
             service = Service(executable_path=GeckoDriverManager().install())
             options = webdriver.FirefoxOptions()
             self.driver = webdriver.Firefox(service=service, options=options)
-        elif browser == "chrome":
+        elif browser == 'chrome':
             service = Service(executable_path=ChromeDriverManager().install())
             options = webdriver.ChromeOptions()
             self.driver = webdriver.Chrome(service=service, options=options)
         self.driver.implicitly_wait(3)
         self.driver.maximize_window()
         self.driver.get(address)
-        time.sleep(testdata["sleep_time"])
+        time.sleep(testdata['sleep_time'])
 
     def find_element(self, mode, path):
-        if mode == "css":
+        if mode == 'css':
             element = self.driver.find_element(By.CSS_SELECTOR, path)
-        elif mode == "xpath":
+        elif mode == 'xpath':
             element = self.driver.find_element(By.XPATH, path)
         else:
             element = None
